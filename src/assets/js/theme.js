482ae63b118675fa53c1214d225026a06c5026c1
Шелл Владислав Евгеньевич <shell-vladislav@mail.ru>
2025-06-20 14:41:23 +0700

    Remove garbage

--- src/assets/js/theme.js
+++
@@ -1,8 +0,0 @@
-// Theme light/dark toggler
-let bodySelector = document.querySelector('body');
-let themeTogglerButton = document.querySelector('.header_theme-toggler');
-
-themeTogglerButton.onclick = function () {
-  bodySelector.classList.toggle('dark-theme');
-  bodySelector.classList.toggle('light-theme');
-};
